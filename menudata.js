afa6abb551256dc01fe4437f96708e1bc014a4ab
CI Builder (PISM manual) <ckhroulev@alaska.edu>
2026-04-08 18:30:08 +0000

    Update the source code browser

--- menudata.js
+++ menudata.js
@@ -79,6 +79,7 @@ var menudata={children:[
 {text:"u",url:"namespacemembers_func_u.html#index_u"},
 {text:"v",url:"namespacemembers_func_v.html#index_v"},
 {text:"w",url:"namespacemembers_func_w.html#index_w"},
+{text:"y",url:"namespacemembers_func_y.html#index_y"},
 {text:"z",url:"namespacemembers_func_z.html#index_z"}]},
 {text:"Variables",url:"namespacemembers_vars.html",children:[
 {text:"a",url:"namespacemembers_vars.html#index_a"},
